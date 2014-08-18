bd53caf2792f62b7fdbd2d9e25cbe11ffa04789f
piotrbernad <piotr.bernad@appunite.com>
2014-08-18 08:53:07 +0200

    Initial Commit

---
+++ AUMedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AUMedia
+//
+//  Created by Piotr Bernad on 18.08.2014.
+//  Copyright (c) 2014 Appunite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
